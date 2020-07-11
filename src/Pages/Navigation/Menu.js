8eef61b6851e515b7642cc3f22a09faa4e3c3057
Caystone99 <57013974+Caystone99@users.noreply.github.com>
2020-07-11 18:32:01 +0100

    Second Debug
    Changes to fontsize and paddings

--- src/Pages/Navigation/Menu.js
+++ src/Pages/Navigation/Menu.js
@@ -6,7 +6,7 @@ class Menu extends Component{
     render(){
         return(
             <nav className='menu'>
-                <span className='nav-logo'><img src='../perroquet-logo.png' width='80px' alt='logo'/></span>
+                <span className='nav-logo'><img src='../perroquet-logo.png' width='120px' alt='logo'/></span>
             </nav>
 
         );
